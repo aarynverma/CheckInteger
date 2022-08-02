1ba2cf5c9f9ee306e7808f12b4560bf5110abfe6
aarynverma <18100BTCSE02667@svvvindore.onmicrosoft.com>
2022-08-02 15:54:56 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace IntegerTypeCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var str = Console.ReadLine();
+            Console.WriteLine("Number:" + typeCheckInt(str) );
+        }
+
+        static bool typeCheckInt(String input)
+        {
+            int number = 0;
+            return int.TryParse(input, out number);
+        }
+    }
+}
